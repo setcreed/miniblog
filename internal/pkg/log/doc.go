804601fb515db67bd7f345461a84d75cd00736b5
setcreed <cwzdzg@foxmail.com>
2024-07-11 11:45:53 +0800

    feat: add log

---
+++ internal/pkg/log/doc.go
@@ -0,0 +1,6 @@
+// # Copyright 2024 setcreed <cwzdzg@foxmail.com>. All rights reserved.
+// # Use of this source code is governed by a MIT style
+// # license that can be found in the LICENSE file. The original repo for
+// # this file is https://github.com/setcreed/miniblog.
+
+package log // import "github.com/setcreed/miniblog/internal/pkg/log"
